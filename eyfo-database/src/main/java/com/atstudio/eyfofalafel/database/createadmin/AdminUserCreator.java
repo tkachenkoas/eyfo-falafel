4ce91c1e10eb664bc58f19530c62be262eba5109
Anton Tkachenko <tkachenkoas@gmail.com>
2019-07-03 23:08:35 +0300

    admin create bean fix

--- eyfo-database/src/main/java/com/atstudio/eyfofalafel/database/createadmin/AdminUserCreator.java
+++ eyfo-database/src/main/java/com/atstudio/eyfofalafel/database/createadmin/AdminUserCreator.java
@@ -31,23 +31,28 @@ public class AdminUserCreator {
 
         String encPassw = encoder.encode(password);
 
-        String insertSql = "INSERT INTO t_user (id, user_name, password) " +
-                            " VALUES ( " +
-                            "   ( SELECT COALESCE (MAX(id), 1) FROM t_user ), :userName, :encPassw " +
-                            " )  " +
-                            " ON CONFLICT ON CONSTRAINT unq_login" +
-                            " DO NOTHING ";
+        log.info("Trying to create admin user {}", userName);
+        try {
+            String insertSql = "INSERT INTO t_user (id, user_name, password) " +
+                    " VALUES ( " +
+                    "   ( SELECT COALESCE (MAX(id) + 1, 1) FROM t_user ), :userName, :encPassw " +
+                    " )  " +
+                    " ON CONFLICT ON CONSTRAINT unq_login" +
+                    " DO NOTHING ";
 
-        int inserted = em.createNativeQuery(insertSql)
-                              .setParameter("userName", userName)
-                              .setParameter("encPassw", encPassw).executeUpdate();
+            int inserted = em.createNativeQuery(insertSql)
+                    .setParameter("userName", userName)
+                    .setParameter("encPassw", encPassw).executeUpdate();
 
-        if (inserted == 0) {
-            log.info("No users were inserted");
-        } else {
-            log.info("Created user {} with password {}", userName, password);
+            if (inserted == 0) {
+                log.info("User {} already exists", userName);
+            } else {
+                log.info("Created user {} with password {}", userName, password);
+            }
+        } catch (Exception e) {
+            log.error("Caught error when trying to create admin user");
+            log.error(e.getMessage(), e);
         }
-
     }
 
 }
