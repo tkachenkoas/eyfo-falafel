ee0f06655c5de568f80c1ce93a958ccb33d2bfe0
Anton Tkachenko <tkachenkoas@gmail.com>
2019-08-04 20:42:15 +0300

    fix groovy script

---
+++ eyfo-database/src/main/resources/create-admin.groovy
@@ -0,0 +1,43 @@
+@GrabConfig( systemClassLoader=true )
+import groovy.sql.Sql
+import org.postgresql.Driver
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+import java.security.SecureRandom
+import java.sql.Connection
+import java.sql.DriverManager
+
+import static java.lang.Integer.parseInt
+
+Properties props = new Properties()
+
+props.load(this.class.classLoader.getResourceAsStream("application.properties") as InputStream)
+props.load(this.class.classLoader.getResourceAsStream("seeders/admin-user.properties") as InputStream)
+
+def encoder = new BCryptPasswordEncoder(
+        parseInt(props."security.password.strength" as String),
+        new SecureRandom(((String)props."security.password.salt").getBytes())
+)
+def encPassw = encoder.encode(props."admin.password" as String)
+
+DriverManager.registerDriver(new Driver())
+Connection conn = DriverManager.getConnection(
+        props."spring.datasource.url" as String,
+        props."spring.datasource.username" as String,
+        props."spring.datasource.password" as String,
+)
+
+def sql = Sql.newInstance(conn)
+
+sql.execute(
+        "INSERT INTO t_user (id, user_name, password) " +
+                " VALUES ( " +
+                "   ( SELECT COALESCE (MAX(id) + 1, 1) FROM t_user ), :userName, :encPassw " +
+                " )  " +
+                " ON CONFLICT ON CONSTRAINT unq_login" +
+                " DO NOTHING ",
+        userName : props."admin.username",
+        encPassw : encPassw
+)
+
+sql.close()
